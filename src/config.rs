30d9fded35a4bd71a89c0ebbc7f76db0200beb37
BLACK_WIDOW\Black Widow <yexinrong18@163.com>
2026-02-04 11:53:34 +0800

    修改第二版

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["61.142.244.87"];
+pub const RS_PUB_KEY: &str = "OBbDqZcc4cQrqkFHF1wUL6iOQbYN90OSDuom+bdDAuM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
